4add03c006624ae328be575680e95201c9f53f5c
Allyson-mkw <allysoneduardo04@gmail.com>
2021-05-23 16:23:03 -0300

    feat: show system location

--- PrimeiroPrograma.java
+++ PrimeiroPrograma.java
@@ -1,7 +1,20 @@
 package com.mycompany.PrimeiroPrograma;
 
+import java.util.Date;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola mundo!");
+        
+        Date today = new Date();
+        System.out.println("Today date is: " + today.toString());
+        
+        String idiom = System.getProperty("user.language"); 
+        System.out.println("Your system language is: " + idiom);
+        
+        String country = System.getProperty("user.country");
+        System.out.println("Your system location is: " + country);
+        
+        
     }
 }
